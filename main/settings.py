d33cba750d3ed10af5f8d5112014e6cc656f24e4
GitHub-Harrison <harrison-johnson@outlook.com>
2022-02-24 17:47:34 +0000

    Install allauth

--- main/settings.py
+++ main/settings.py
@@ -44,11 +44,20 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'library',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECt_URL = '/'
+LOGOUT_REDIRECt_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
