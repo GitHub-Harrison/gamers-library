cdd01843a9e3c896b791b019eb7deb5698c761cb
GitHub-Harrison <harrison-johnson@outlook.com>
2022-02-26 17:06:42 +0000

    Create base.html and home.html templates

--- home/views.py
+++ home/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, random words haha hewho.")
+
+def home(response):
+    return render(response, './home.html')
