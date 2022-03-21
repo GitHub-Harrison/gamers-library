19b263861f8cf74325ab42d7560c4bae2191f62a
GitHub-Harrison <harrison-johnson@outlook.com>
2022-03-21 11:58:06 +0000

    Create AddGameForm class for form

--- add_game/forms.py
+++ add_game/forms.py
@@ -1,3 +1,17 @@
 from django import forms
+from library.models import Post
 
-# class AddGameForm(forms.Form):
+
+class AddGameForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ['title', 'image', 'description', 'genre', 'platform' , 'release_date']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'pad'}),
+            'image': forms.FileInput(attrs={'class': 'pad'}),
+            'description': forms.Textarea(attrs={'class': 'pad', 'cols': 60, 'rows': 5}),
+            'genre': forms.Select(choices=Post.GENRES, attrs={'class': 'pad'}),
+            'platform': forms.Select(choices=Post.GAMING_PLATFORM, attrs={'class': 'pad'}),
+            'release_date': forms.DateInput(attrs={'class': 'pad'})
+        }
