f21bf9ea495968f62637d4ca6fa655ca797e56f5
GitHub-Harrison <harrison-johnson@outlook.com>
2022-03-21 08:17:26 +0000

    Add views for add game form

--- add_game/views.py
+++ add_game/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from .forms import AddGameForm
 
 # Create your views here.
+
+
+# def post_game(request):
+#     if request.method == 'POST':
+#         form = AddGameForm(request.POST)
+#         if form.is_valid():
+#             return HttpResponseRedirect('/library/')
 
 
 def add_game(response):
