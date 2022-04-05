0faa2d61c8bf7ecc08f7f6d43074249534ee095e
GitHub-Harrison <harrison-johnson@outlook.com>
2022-04-05 14:08:43 +0000

    Comment out all post detail related code

--- library/urls.py
+++ library/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('library/', views.library, name='library'),
+    # path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
