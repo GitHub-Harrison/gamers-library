c7986c248eda62f75bb3ca9566da2a06c0d4b68f
GitHub-Harrison <harrison-johnson@outlook.com>
2022-04-05 15:11:36 +0000

    Comment out all add comment related code for future changes

--- library/models.py
+++ library/models.py
@@ -66,19 +66,19 @@ class Post(models.Model):
         return self.title
 
 
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name="comments")
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
-    likes = models.ManyToManyField(
-        User, related_name='blogpost_like', blank=True)
+# class Comment(models.Model):
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE,
+#                              related_name="comments")
+#     name = models.CharField(max_length=80)
+#     email = models.EmailField()
+#     body = models.TextField()
+#     created_on = models.DateTimeField(auto_now_add=True)
+#     approved = models.BooleanField(default=False)
+#     likes = models.ManyToManyField(
+#         User, related_name='blogpost_like', blank=True)
 
-    class Meta:
-        ordering = ["created_on"]
+#     class Meta:
+#         ordering = ["created_on"]
 
-    def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+#     def __str__(self):
+#         return f"Comment {self.body} by {self.name}"
--- library/views.py
+++ library/views.py
@@ -22,23 +22,23 @@ def library(response):
     return render(response, template, context)
 
 
-class PostDetail(View):
+# class PostDetail(View):
 
-    def get(self, request, slug, *args, **kwargs):
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('created_on')
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+#     def get(self, request, slug, *args, **kwargs):
+#         queryset = Post.objects.filter(status=1)
+#         post = get_object_or_404(queryset, slug=slug)
+#         comments = post.comments.filter(approved=True).order_by('created_on')
+#         liked = False
+#         if post.likes.filter(id=self.request.user.id).exists():
+#             liked = True
 
-        return render(
-            request,
-            "library/post_detail.html",
-            {
-                "post": post,
-                "comments": comment,
-                "liked": liked,
-                "comment_form": CommentForm()
-            }
-        )
+#         return render(
+#             request,
+#             "library/post_detail.html",
+#             {
+#                 "post": post,
+#                 "comments": comment,
+#                 "liked": liked,
+#                 "comment_form": CommentForm()
+#             }
+#         )
