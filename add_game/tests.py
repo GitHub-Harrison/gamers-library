c047f857e3437f431fbcb23274476a2a18e86efc
GitHub-Harrison <harrison-johnson@outlook.com>
2022-04-20 16:41:57 +0000

    Add url test

--- add_game/tests.py
+++ add_game/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class UrlTest(TestCase):
+
+    def testLibraryPage(self):
+        response = self.client.get('add_game/')
+        print(response)
+
+        self.assertEqual(response.status_code, 200)
