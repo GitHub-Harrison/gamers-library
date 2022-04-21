e85881d91042932a1255f73fb07cfe43c498b3f9
GitHub-Harrison <harrison-johnson@outlook.com>
2022-04-21 11:59:11 +0000

    Create a sample env.py file

---
+++ env_sample.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", "<your secret key here>")
+os.environ.setdefault("DEBUG", "<true>")
+os.environ.setdefault("DATABASE_URL", "<your database url>")
+os.environ.setdefault("CLOUDINARY_URL", "<your cloudinary url>")
