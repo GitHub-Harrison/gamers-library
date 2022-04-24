b427fcbecf2a6e96f84bde3d3189c72027c0f40f
GitHub-Harrison <harrison-johnson@outlook.com>
2022-04-24 18:40:02 +0000

    Rename test to TestHomePage

--- home/tests.py
+++ home/tests.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 
 class UrlTest(TestCase):
 
-    def testLibraryPage(self):
-        response = self.client.get('')
+    def testHomePage(self):
+        response = self.client.get('/')
         print(response)
 
         self.assertEqual(response.status_code, 200)
