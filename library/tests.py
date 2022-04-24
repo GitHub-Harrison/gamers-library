2e492b541580ed825b0c697b6380d80fd4c35682
GitHub-Harrison <harrison-johnson@outlook.com>
2022-04-24 18:40:51 +0000

    Add test for Post model str

--- library/tests.py
+++ library/tests.py
@@ -1,8 +1,15 @@
 from django.test import TestCase
+from .models import Post, Comment
+
+
+class PostTest(TestCase):
+
+    def test_post_model_str(self):
+        title = Post.objects.create(title="Django Testing")
+        self.assertEqual(str(title), "Django Testing")
 
 
 class UrlTest(TestCase):
-
     def testLibraryPage(self):
         response = self.client.get('library/')
         print(response)
