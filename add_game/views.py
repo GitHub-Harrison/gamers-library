7219a1c5d421f07a4db43e9ba70d112cb2b03117
GitHub-Harrison <harrison-johnson@outlook.com>
2022-04-27 12:33:42 +0000

    Add messages for the user to see if the request was a success or not

--- add_game/views.py
+++ add_game/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import AddGameForm
 from library.views import library
+from django.contrib import messages
 
 
 def add_game(request):
@@ -8,7 +9,10 @@ def add_game(request):
         form = AddGameForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            messages.success(request, "Game successfully added")
             return redirect(library)
+        else:
+            messages.error(request, "There was a problem adding this game. Please try again")
     form = AddGameForm()
     template = 'add_game/add_game.html'
     context = {
