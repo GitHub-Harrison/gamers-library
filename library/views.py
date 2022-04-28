15ee8a74bb088f1df60ca8fd456bb4e8d4b0ee4e
GitHub-Harrison <harrison-johnson@outlook.com>
2022-04-28 14:59:27 +0000

    Add mock UpdateComment class view

--- library/views.py
+++ library/views.py
@@ -57,3 +57,9 @@ def post_detail(request, slug):
     }
 
     return render(request, template, context)
+
+
+# class UpdateComment():
+#     model = CommentForm
+#     template = library/update_comment.html
+#     fields = ['body']
