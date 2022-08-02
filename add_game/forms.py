21613dfc2e4dfb5fa065e9eddb81b1d693883b0c
GitHub-Harrison <harrison-johnson@outlook.com>
2022-08-02 11:44:50 +0000

    Add slug and formatting to form

--- add_game/forms.py
+++ add_game/forms.py
@@ -2,16 +2,26 @@ from django import forms
 from library.models import Post
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class AddGameForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['title', 'image', 'description', 'genre', 'platform', 'release_date']
+        fields = [
+            'title', 'image', 'description', 'genre',
+            'platform', 'release_date', 'slug'
+            ]
         widgets = {
             'title': forms.TextInput(attrs={'class': 'pad'}),
             'image': forms.FileInput(attrs={'class': 'pad'}),
-            'description': forms.Textarea(attrs={'class': 'pad', 'cols': 45, 'rows': 5}),
+            'description': forms.Textarea(
+                attrs={'class': 'pad', 'cols': 45, 'rows': 5}),
             'genre': forms.Select(choices=Post.GENRES, attrs={'class': 'pad'}),
-            'platform': forms.Select(choices=Post.GAMING_PLATFORM, attrs={'class': 'pad'}),
-            'release_date': forms.DateInput(attrs={'class': 'pad', 'placeholder': 'Month Day, Year (mmm dd, yyyy)'})
+            'platform': forms.Select(
+                choices=Post.GAMING_PLATFORM, attrs={'class': 'pad'}),
+            'release_date': DateInput(attrs={'class': 'pad'}),
+            'slug': forms.HiddenInput(),
         }
