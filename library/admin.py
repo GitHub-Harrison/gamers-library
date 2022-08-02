cbb8e33b9d91bd504091a17e421b7890ceb86416
GitHub-Harrison <harrison-johnson@outlook.com>
2022-08-02 12:21:12 +0000

    Add admin styling for posts

--- library/admin.py
+++ library/admin.py
@@ -2,15 +2,23 @@ from django.contrib import admin
 from .models import Post, Comment
 
 
-admin.site.register(Post)
-admin.site.register(Comment)
-
-
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'body' 'post', 'created_on', 'active')
+    list_display = ('user', 'body', 'post', 'created_on', 'active')
     list_filter = ('active', 'created_on')
-    search_fields = ('name', 'email', 'body')
+    search_fields = ('user', 'body')
     actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
         queryset.update(active=True)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'title', 'genre', 'platform', 'release_date',
+        'approved', 'created_on')
+    list_filter = ('approved', 'genre', 'platform')
+    search_fields = ('user', 'description', 'title')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
